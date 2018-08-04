44f31e9be50b6d9609a929da6ea8e1f381c6919d
agata okolska <agata.okolska@gmail.com>
2018-08-04 14:30:33 +0200

    add components: Form, List,Task and ToDo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import ToDo from './components/ToDo'
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      
+        <ToDo />
       </div>
     )
   }
---
+++ src/components/ToDo/ToDo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+class ToDo extends React.Component {
+    render() {
+        return (
+            <div>
+                <Form />
+                <List />
+            </div>
+        )
+    }
+}
+export default ToDo
