1a7ef4de8c2a5204c33f7fb59d4bcc7115b521c4
agata okolska <agata.okolska@gmail.com>
2018-08-04 14:45:25 +0200

    add MuiThemeProvider to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
-<App />, 
-document.getElementById('root'))
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>,
+    document.getElementById('root'))
